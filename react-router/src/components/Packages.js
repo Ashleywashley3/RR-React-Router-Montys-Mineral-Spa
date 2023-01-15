c5f39adabaca32812487210c9cf2c893416012e7
Ashleywashley3 <ash.mcmaken@gmail.com>
2023-01-15 13:08:59 -0500

    initial commit

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,16 @@
 
 
-export default function Packages() {
-
+export default function Packages({ packages }) {
+    const display = packages.map((elem, i) => {
+        return <li key={i}>{elem}</li>
+    })
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                 {display}
             </div>
         </div>
     )
